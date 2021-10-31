f1dfc8375eb03c2c89878922e6225fe3b4728b73
Roman Bachmann <Roman.Bachmann@swisscom.com>
2021-10-31 22:01:20 +0100

    Drop 'legacy' from project name as current generation of MyStrom switches is supported too

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -rf build/libs
 ./gradlew assemble
-docker build --platform linux/arm64 -t robachmann/mystrom-legacy-exporter:1.1.1-arm64 .
-docker push robachmann/mystrom-legacy-exporter:1.1.1-arm64
+docker build --platform linux/arm64 -t robachmann/mystrom-exporter:1.1.1-arm64 .
+docker push robachmann/mystrom-exporter:1.1.1-arm64
